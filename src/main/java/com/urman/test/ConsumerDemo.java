b92ce55da3c7f114c2b0c53b04bb8bc3439e3869
Urman Ratneshwar <urmanhr@gmail.com>
2022-07-27 20:54:30 +0530

    IntStream sample

--- src/main/java/com/urman/test/ConsumerDemo.java
+++ src/main/java/com/urman/test/ConsumerDemo.java
@@ -3,11 +3,16 @@ package com.urman.test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ConsumerDemo {
 	
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(1,2,3,4);
+		
+		//sample IntStream
+		list = IntStream.range(0, 100).boxed().collect(Collectors.toList());
 //		Consumer<Integer> consumer = System.out::println;
 		Consumer<Integer> consumer = new Consumer<Integer>() {
 			
